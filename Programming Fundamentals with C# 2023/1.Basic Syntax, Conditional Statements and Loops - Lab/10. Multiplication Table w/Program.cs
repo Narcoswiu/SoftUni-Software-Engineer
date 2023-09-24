1cc6d6fa0d3fdd41217ba4eae40ad47738d8219e
Narcoswiu <narcoswiu324@gmail.com>
2023-09-24 13:29:40 +0300

    New Exercise

---
+++ Programming Fundamentals with C# 2023/1.Basic Syntax, Conditional Statements and Loops - Lab/10. Multiplication Table w/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10.Multiplication_Table_w
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          int number=int.Parse(Console.ReadLine());
+            int product = 0;
+            for (int i = 1; i <= 10; i++)
+            {
+                product = number * i;
+                Console.WriteLine($"{number} X {i} = {product}");
+            }
+            
+        }
+    }
+}
