cf036cb6d7dafa09a97e61042487680e4eb8ea1d
Narcoswiu <narcoswiu324@gmail.com>
2023-09-24 13:37:00 +0300

    New Exercise

---
+++ Programming Fundamentals with C# 2023/1.Basic Syntax, Conditional Statements and Loops - Lab/12. Even Number/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12.Even_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            while (true)
+            {
+                if (number % 2 == 0)
+                {
+                    Console.WriteLine("The number is: {0}", Math.Abs(number), number);
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Please write an even number.");
+                    number = int.Parse(Console.ReadLine());
+                }
+
+            }
+
+
+        }
+    }
+}
